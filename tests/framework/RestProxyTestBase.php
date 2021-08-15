182cb1b54024f4c4397d072cf4fab7d23dd0ce1b
Basel <basel@gmx.co.uk>
2021-08-16 00:39:11 +0100

    Return the UUID

--- tests/framework/RestProxyTestBase.php
+++ tests/framework/RestProxyTestBase.php
@@ -98,6 +98,6 @@ class RestProxyTestBase extends TestCase
      */
     protected function generateGUID(): string
     {
-        sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+        return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
     }
 }
